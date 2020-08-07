88e5b259df8aaaffe01eb45b98a170b7d8d46a03
Tim Williamson <timothy.williamson@santander.co.uk>
2020-08-07 09:44:26 +0100

    chore(webpack-tweaks) remove minification temporarily to allow gpu code to work (to find another solution)

--- webpack-tweaks.config.js
+++ webpack-tweaks.config.js
@@ -1,4 +1,15 @@
-module.exports.externals = {
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = cfg => {
+  cfg.externals = {
     fs: '{}',
     perf_hooks: '{performance: performance}'
   }
+  cfg.optimization.minimizer.filter (({constructor: {name}}) => name === 'TerserPlugin')
+  .forEach (terser => {
+    terser.options.terserOptions.compress=false;
+    terser.options.terserOptions.mangle=false;
+  });
+  
+  return cfg;
+}
